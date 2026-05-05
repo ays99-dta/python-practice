281e46e36b15b1e3fff697536b4244117f0ed460
Ahmed Yasser <ahmedyassersayed.eg@gmail.com>
2026-05-05 15:15:25 +0300

    Week 2: Decisions and loops (control flow)

--- week1/seconds_of_life_2_trial.py
+++ week1/seconds_of_life_2_trial.py
@@ -4,6 +4,7 @@ dob=dt.strptime(input("Enter your date of birth DD/MM/YYYY: "),"%d/%m/%Y")
 diff=now-dob
 print(f"The number of seconds since your date of birth is {diff.total_seconds() :.2f}.")
 
+
 """
 - First of all is .strptime() which parses stringes. It converts the string input into a datetime object that could be worked with.
 - The %d/%m/%Y is the formmat of datetime that gets us DD/MM/YYYY 
